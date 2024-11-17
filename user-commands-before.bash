012f6dfe72661aa316fe7162de9397504710070e
DarkSirrush <79956291+DarkSirrush@users.noreply.github.com>
2024-11-17 13:03:54 -0800

    Update user-commands-before.bash

--- user-commands-before.bash
+++ user-commands-before.bash
@@ -1,4 +1,4 @@
-cat << EOF >> /etc/pacman.conf
+sudo cat << EOF >> /etc/pacman.conf
 [lizardbyte-beta]
 SigLevel = Optional
 Server = https://github.com/LizardByte/pacman-repo/releases/download/beta
